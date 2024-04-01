24b57b9a7b0442490f732b451900a418df1153aa
Olivier Van Ransbeeck <olivier.vanransbeeck@hotmail.com>
2024-04-01 16:11:48 +0200

    modified: Corsoptions4

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
+    "https://gameroomexperienceapp-vhb.onrender.com",
+    "http://localhost:5173"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
